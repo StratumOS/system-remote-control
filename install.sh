210677166d67f972401e1369ff57703c93e65dca
System 66 <info@system66.com>
2018-07-10 23:19:58 +0100

    update

--- install.sh
+++ install.sh
@@ -4,5 +4,40 @@ PAUSE=1
 
 # checking and installing dependencies
 
-apt -y install screen
+# install screen strace
+apt -y install screen strace
 
+# create screen service
+
+cat > /etc/systemd/system/autostart.service << EOF
+[Unit]
+Description = Autostart Captain
+After = NetworkManager-wait-online.service network.target network-online.target dbus.service
+Wants = NetworkManager-wait-online.service network-online.target
+Requires = dbus.service
+Documentation=https://github.com/system66
+
+[Service]
+Type = forking
+PIDFile = /var/run/screen-captain.pid
+ExecStart = /usr/bin/screen -dmS xmr-stak xmr-stak --config /opt/stratumos/xmr-stak/config.txt --poolconf /opt/stratumos/xmr-stak/pools.txt
+WorkingDirectory=/opt/stratumos/xmr-stak
+Restart = on-abort
+
+[Install]
+WantedBy = multi-user.target
+
+EOF
+
+# to ensure the system boots to the console (TTY) rather than to a display manager
+systemctl set-default multi-user.target
+
+systemctl enable autostart.service
+systemctl daemon-reload
+
+# start autosatart.service
+systemctl start autostart.service
+
+
+chkconfig --list apex
+chkconfig --add apex
